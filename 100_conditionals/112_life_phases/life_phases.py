4b1aa260edb0828f95bba9198c2e5bb711bb5875
mAbduqayum <mabduqayum@gmail.com>
2025-09-27 12:35:29 +0500

    fix: solution

--- 100_conditionals/112_life_phases/life_phases.py
+++ 100_conditionals/112_life_phases/life_phases.py
@@ -2,17 +2,17 @@ age = int(input())
 
 if age < 0:
     print("Invalid age")
-elif age <= 2:
+elif age < 2:
     print("Infant")
-elif age <= 5:
+elif age < 5:
     print("Toddler")
-elif age <= 11:
+elif age < 11:
     print("Child")
-elif age <= 18:
+elif age < 18:
     print("Adolescent")
-elif age <= 40:
+elif age < 40:
     print("Young Adult")
-elif age <= 65:
+elif age < 65:
     print("Middle-aged")
 else:
     print("Senior")
