859b131b53b5bbc2bd327a752ee68e8f77624962
mAbduqayum <mabduqayum@gmail.com>
2025-09-29 12:37:35 +0500

    ref:

--- 100_conditionals/138_next_day/next_day.py
+++ 100_conditionals/138_next_day/next_day.py
@@ -2,14 +2,15 @@ year = int(input())
 month = int(input())
 day = int(input())
 
-is_leap = year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
-
-if month == 2:
-    days_in_month = 29 if is_leap else 28
-elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-    days_in_month = 31
-else:
-    days_in_month = 30
+days_in_month = 31
+match month:
+    case 2:
+        days_in_month = 28
+        is_leap = year % 400 == 0 or year % 4 == 0 and year % 100 != 0
+        if is_leap:
+            days_in_month += 1
+    case 4 | 6 | 9 | 11:
+        days_in_month = 30
 
 next_day = day + 1
 next_month = month
@@ -22,7 +23,4 @@ if next_day > days_in_month:
         next_month = 1
         next_year += 1
 
-month_str = f"{next_month:02d}"
-day_str = f"{next_day:02d}"
-
-print(f"{next_year}-{month_str}-{day_str}")
+print(f"{next_year}-{next_month:02d}-{next_day:02d}")
