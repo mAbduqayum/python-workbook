4446e60dad46005026d5616794529b4defb094ee
mAbduqayum <mabduqayum@gmail.com>
2025-10-11 23:48:39 +0500

    ref:

--- 000_intro/036_ideal_gas/ideal_gas.py
+++ 000_intro/036_ideal_gas/ideal_gas.py
@@ -1,6 +1,7 @@
 pressure = float(input("Enter pressure (Pascals): "))
-volume = float(input("Enter volume (liters): "))
+volume_liters = float(input("Enter volume (liters): "))
 temp_celsius = float(input("Enter temperature (°C): "))
 temp_kelvin = temp_celsius + 273.15
+volume = volume_liters / 1000  # cubic
 n = (pressure * volume) / (8.314 * temp_kelvin)
 print(f"Amount of gas: {n:.2f} moles")
