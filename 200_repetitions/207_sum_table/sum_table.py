64603966ebb520d8881c8af4fd3918daa77f0643
mAbduqayum <mabduqayum@gmail.com>
2025-10-11 13:36:33 +0500

    ref:

--- 200_repetitions/207_sum_table/sum_table.py
+++ 200_repetitions/207_sum_table/sum_table.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-for row in range(1, n + 1):
-    for col in range(1, n + 1):
+for row in range(n + 1):
+    for col in range(n + 1):
         value = row + col
         print(f"{value:2}", end="")
         if col < n:
