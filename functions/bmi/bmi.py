519602e842235f725f9060703885e0683a7c152d
mAbduqayum <mabduqayum@gmail.com>
2026-05-22 16:56:17 +0500

    ref:

--- functions/bmi/bmi.py
+++ functions/bmi/bmi.py
@@ -1,2 +1,10 @@
 def bmi(weight: float, height: float) -> float:
     return weight / (height**2)
+
+
+if __name__ == "__main__":
+    # Test your function
+    print(bmi(70, 1.75))  # 22.857142857142858
+    print(bmi(80, 1.80))  # 24.691358024691358
+    print(bmi(50, 1.60))  # 19.53125
+    print(bmi(90, 1.75))  # 29.387755102040817
--- functions/is_triangle/is_triangle.py
+++ functions/is_triangle/is_triangle.py
@@ -4,8 +4,8 @@ def is_triangle(a: float, b: float, c: float) -> bool:
 
 if __name__ == "__main__":
     # Test your function
-    is_triangle(3, 4, 5)  # True
-    is_triangle(1, 2, 3)  # False
-    is_triangle(5, 5, 5)  # True
-    is_triangle(1, 1, 10)  # False
-    is_triangle(7, 10, 5)  # True
+    print(is_triangle(3, 4, 5))  # True
+    print(is_triangle(1, 2, 3))  # False
+    print(is_triangle(5, 5, 5))  # True
+    print(is_triangle(1, 1, 10))  # False
+    print(is_triangle(7, 10, 5))  # True
